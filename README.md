796d2fcf61fc1e8cf5cecdb8c2e4b8b634ac0681
simone46b <simone46bruenxfh@hotmail.com>
2025-08-28 12:05:56 +0000

    upload codebase

---
+++ README.md
@@ -0,0 +1,7 @@
+# EVM Pumpfun(pump.fun) smart contract
+
+Pumpfun(pump.fun) smart contract on evm - fork pump.fun pump bonding curve logic for meme ccoin launch pad(support monda, base, bnb, ethereum layer2-xlayer) - Solidity Smart Contact For ape.store.
+
+## Contact 
+
+If you have any question, contact here: [Telegram](https://t.me/satscorder) | [Twitter](https://x.com/satcorder33)
---
+++ contracts/Lock.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Lock {
+    uint public unlockTime;
+    address payable public owner;
+
+    event Withdrawal(uint amount, uint when);
+
+    constructor(uint _unlockTime) payable {
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time should be in the future"
+        );
+
+        unlockTime = _unlockTime;
+        owner = payable(msg.sender);
+    }
+
+    function withdraw() public {
+        // private
+    }
+}
